a269dca8a46141026acd6719febfbd58795ddb78
Marius Kittler <mkittler@suse.de>
2025-01-29 11:20:28 +0100

    Revert "Allow specifying the timeout on `get_cmd_output` calls"
    This reverts commit ca4f085c3e8baf01190c1f4d160edba7109d66a6.
    This reverts commit 0d09ec97e6c84068e2f9911c1d0229f991484a15.

--- backend/baseclass.pm
+++ backend/baseclass.pm
@@ -28,7 +28,6 @@ use List::Util 'min';
 use List::MoreUtils 'uniq';
 use Scalar::Util 'looks_like_number';
 use Mojo::File 'path';
-use Mojo::Util 'scope_guard';
 use OpenQA::Exceptions;
 use Time::Seconds;
 use English -no_match_vars;
@@ -1302,6 +1301,7 @@ sub check_ssh_serial ($self, $fh = undef, $write = undef) {
 
 =cut
 sub run_ssh_cmd ($self, $cmd, %args) {
+    my ($stdout, $stderr) = ('', '');
     $args{wantarray} //= 0;
     $args{keep_open} //= 1;
 
@@ -1309,29 +1309,15 @@ sub run_ssh_cmd ($self, $cmd, %args) {
     my ($ssh, $chan) = $self->run_ssh($cmd, %args);
     $chan->send_eof;
 
-    my ($stdout, $stderr) = ('', '');
-    my $log_output = sub () {
-        bmwqemu::diag("[run_ssh_cmd($cmd)] stdout:$/$stdout") if length $stdout;
-        bmwqemu::diag("[run_ssh_cmd($cmd)] stderr:$/$stderr") if length $stderr;
-    };
-    my $timeout_guard;
-    if (defined(my $new_timeout = $args{timeout})) {
-        my $initial_timeout = $ssh->timeout;
-        $timeout_guard = scope_guard sub { $ssh->timeout($initial_timeout) };
-        $ssh->timeout($new_timeout);
-    }
     until ($chan->eof) {
         if (my ($o, $e) = $chan->read2) {
             $stdout .= $o;
             $stderr .= $e;
         }
-        else {
-            $log_output->();
-            $ssh->die_with_error;
-        }
     }
 
-    $log_output->();
+    bmwqemu::diag("[run_ssh_cmd($cmd)] stdout:$/$stdout") if length($stdout);
+    bmwqemu::diag("[run_ssh_cmd($cmd)] stderr:$/$stderr") if length($stderr);
     my $ret = $chan->exit_status();
     bmwqemu::diag("[run_ssh_cmd($cmd)] exit-code: $ret");
     $ssh->disconnect() unless $args{keep_open};
